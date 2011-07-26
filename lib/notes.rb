c60481b2a04ebf9589defcef516063cda4cd30f8
Lyon <lyon@delorum.com>
2011-07-26 09:10:06 -0600

    reading file in is easy!

--- lib/notes.rb
+++ lib/notes.rb
@@ -3,25 +3,39 @@ require "notes/version"
 module Notes
   # Your code goes here...
 
+  def check_file
+    `touch #{notes}` unless File.exist? notes
+  end
+
+  def running_on_windows?
+    RUBY_PLATFORM =~ /mswin32|mingw32/
+  end
+
+
+  def notes
+    "#{ENV['HOME']}/.notes"
+  end
+
   def get_hash
-    # read file
-    # convert string to hash
-    hash = {"key" => "value"}
-    eval(File.open("~/.file", "r").read)
-    
+    check_file
+    eval(File.open(notes, 'r') { |f| f.read })
   end
 
   def save_hash(hash)
-    
+    puts "SHOULD BE SAVING #{hash}"
   end
 
-
   def create(key)
-    puts "what value do you want to assign to this key?"
-    string = IOSOEMTHING.gets.strip
+    print "what value do you want to assign to this key? "
+    string = STDIN.gets.strip
     hash = get_hash
+    hash = {} if hash == nil
     hash[key] = string
     save_hash hash
+  end
+  
+  def destroy(key)
+    
   end
   
   def list
@@ -30,8 +44,12 @@ module Notes
     end
   end
 
-  def hello
-    puts "hello"
+  def help
+    puts ""
+    puts "-l         Lists all note names"
+    puts "-c <name>  Create a new note"
+    puts "-d <name>  Delete a existing note"
+    puts ""
   end
 
 end
